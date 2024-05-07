bcafec6a89257eb71155b1c5ff2b6305d3e4c792
ducknh <149159294+ducknh@users.noreply.github.com>
2024-05-07 16:02:55 +0700

    Delete src directory

--- src/main/java/librarymanagement/getData.java
+++
@@ -1,9 +0,0 @@
-package librarymanagement;
-
-public class getData {
-
-    public static String path;
-    public static String studentNumber;
-
-    public static String pathImage;
-}
--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module librarymanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens librarymanagement to javafx.fxml;
-    exports librarymanagement;
-}
