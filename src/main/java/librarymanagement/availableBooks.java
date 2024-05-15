19b597819eca8fcd20bdff52c8e8df55fc06030b
ADMIN <khanhquan1102@gmail.com>
2024-05-15 18:03:55 +0700

    studentdashboard

---
+++ src/main/java/librarymanagement/availableBooks.java
@@ -0,0 +1,40 @@
+package librarymanagement;
+
+import java.util.Date;
+
+public class availableBooks {
+    private final String title;
+    private final String author;
+    private final String genre;
+    private final String image;
+    private final Date date;
+
+    public availableBooks(String title, String author, String genre, String image, Date date){
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.image = image;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+}
