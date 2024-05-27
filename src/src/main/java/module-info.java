9600549d3ed8407ed47192a23f1e6b44cfb2b07a
ducknh <149159294+ducknh@users.noreply.github.com>
2024-05-27 12:06:38 +0700

    test

--- src/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module librarymanagement {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-    requires fontawesomefx;
-
-    opens librarymanagement to javafx.fxml;
-    exports librarymanagement;
-}
